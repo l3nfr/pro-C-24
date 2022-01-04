23648f031570a370df0b7ca3d1208ecf7ff8ff63
l3nfr <76814774+l3nfr@users.noreply.github.com>
2022-01-04 19:59:33 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,28 @@
+class Iron{
+	constructor(x,y,r)
+	{
+	var options={
+		'restitution': 0.8,
+		'friction': 3,
+		'density': 30
+	}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+    {
+        var ironpos=this.body.position;		
+        push()
+        translate(ironpos.x, ironpos.y);
+        rectMode(CENTER)
+        strokeWeight(4);
+        stroke("black");
+        fill("darkblue");
+
+        pop();
+    }
+}
